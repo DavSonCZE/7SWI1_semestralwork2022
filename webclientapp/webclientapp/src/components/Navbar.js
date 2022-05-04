40edee2a4a7c9daa51591643273188eb7cdd1f67
DavSonCZE <40635601+DavSonCZE@users.noreply.github.com>
2022-05-04 19:46:20 +0200

    kkk

---
+++ webclientapp/webclientapp/src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar(){
+    return(
+        <div className="navbar">
+            <div>Seznam her</div>
+            <div>Něco</div>
+            <div>Něco 2</div>
+        </div>
+    )
+}
